4c65cd1ce1771d5b77e20a2a2f63ed598c81f820
DiegoPizarro <DI.PIZARROV@duocuc.cl>
2025-12-05 23:10:05 -0300

    Update main.py

--- CGUIDE_API/main.py
+++ CGUIDE_API/main.py
@@ -8,8 +8,8 @@ import os
 app = FastAPI()
 
 DB_USER = "CALAMITYDEV"
-DB_PASSWORD = "Contrasena12"
-DSN_NAME = "om7h1y6krf21abpa_low"
+DB_PASSWORD = "Tu contraseña aqui"
+DSN_NAME = "DSN AQUI"
 WALLET_DIR = os.path.join(os.getcwd(), "wallet")
 
 # modelos
@@ -38,7 +38,7 @@ def get_db_connection():
         dsn=DSN_NAME,
         config_dir=WALLET_DIR,
         wallet_location=WALLET_DIR,
-        wallet_password="Paraset653987"
+        wallet_password="Contraseña aqui"
     )
 
 # endpoints
